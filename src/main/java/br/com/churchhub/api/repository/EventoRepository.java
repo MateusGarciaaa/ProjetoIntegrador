12322e380855ededb8b33933142f116e8a69fb1d
MateusGarciaaa <mateusgarcia3081@gmail.com>
2026-08-12 09:02:01 -0300

    feat: add method to retrieve top 5 upcoming events by date

--- src/main/java/br/com/churchhub/api/repository/EventoRepository.java
+++ src/main/java/br/com/churchhub/api/repository/EventoRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventoRepository extends JpaRepository<Evento, UUID> {
     Page<Evento> findByDataBetween(LocalDate dataInicio, LocalDate dataFim, Pageable pageable);
+
+    List<Evento> findTop5ByDataGreaterThanEqualOrderByDataAscHorarioAsc(LocalDate data);
 }
